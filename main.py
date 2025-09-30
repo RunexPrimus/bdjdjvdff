8453463774917ea49efb914441f06b4f4f1b6ae4
Runex <runex.primus@gmail.com>
2025-09-30 20:26:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ logger = logging.getLogger(__name__)
 # ---------------- ENV ----------------
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@SizningKanal")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@Digen_Ai")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001234567890"))
 DIGEN_KEYS = json.loads(os.getenv("DIGEN_KEYS", "[]"))
 DIGEN_URL = os.getenv("DIGEN_URL", "https://api.digen.ai/v2/tools/text_to_image")
